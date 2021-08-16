a573ccb69e6ad0e385c2a9a92b8b300038d6897a
umuş <seyitumutbicer@gmail.com>
2021-08-17 00:05:48 +0300

    dev

--- src/core/modify/set_mixin.js
+++ src/core/modify/set_mixin.js
@@ -1,8 +1,9 @@
 module.exports = {
    name: "set_mixin",
    function: async function (payload, ctx) {
-      for (let i of payload.body.mixin || []) {
-         payload.body = ctx.deepMerge(payload.body, ctx.local.get("mixin",i).object)
+      let arr = payload.body.mixin
+      for (let i of arr) {
+         payload.body = ctx.deepMerge(payload.body, ctx.local.get("mixin", i).object)
       }
    }
 }
